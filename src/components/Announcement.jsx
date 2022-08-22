4d3ce0bedabe75fb8743f6bbeee77d2d8c0456fb
RogersBobMunyenga <b.munyenga@gmail.com>
2022-08-22 20:14:41 +0300

    annoucement bar style update

--- src/components/Announcement.jsx
+++ src/components/Announcement.jsx
@@ -7,6 +7,8 @@ const Container = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
+    font-size: 14px;
+    font-weight: 500;
 `;
 const Announcement = () => {
   return (
